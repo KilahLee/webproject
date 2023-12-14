43ae484dd02b70f4391ca46aadc01fe9247ae4a0
Gilah <kilah@naver.com>
2023-12-14 12:40:24 +0900

    BMI 측정일 추가 bmi 계산 결과 출력값 소수자리 제어

--- src/main/java/net/sesoc/web1/controller/MainController.java
+++ src/main/java/net/sesoc/web1/controller/MainController.java
@@ -1,5 +1,9 @@
 package net.sesoc.web1.controller;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +21,19 @@ public class MainController {
 			@RequestParam(name = "height") double height, 
 			@RequestParam(name = "weight") double weight,
 			Model model) {
+		
+		// 현재 날짜기능 추가 및 bmi값의 출력포맷을 수정
+		LocalDateTime today= LocalDateTime.now();
+		String now = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+				       .format(today);
+		
 		double temp = height * 0.01;
 		double bmi = weight / Math.pow(temp, 2);
-		System.out.println(bmi);
-		model.addAttribute("bmi", bmi);
+		String userBMI = String.format("%.2f", bmi);
+		
+		model.addAttribute("now", now);
+		model.addAttribute("bmi", userBMI);
+		
 		return "index";
 	}
 }
